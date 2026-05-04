60b5814d57bf02f936febcdcc412aecb00fb603e
MEHMET <64442588+mhmtlger12@users.noreply.github.com>
2026-05-04 09:30:57 +0300

    Add User, Department, Permission, Menu CRUD pages with tree views

---
+++ Baselib.Presentation/Pages/Admin/Departments/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Baselib.Presentation.Pages.Admin.Departments;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
---
+++ Baselib.Presentation/Pages/Admin/Menus/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Baselib.Presentation.Pages.Admin.Menus;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
