d2aa3a9d33c182f52224d237b9f54890f9690f89
Mehmet Güler <gitmeturnam_mehmet@hotmail.com>
2026-09-20 21:07:53 +0300

    Remove Baselib.Presentation project from the solution file

--- Baselib.Presentation/Models/ApiResponse.cs
+++
@@ -1,9 +0,0 @@
-namespace Baselib.Presentation.Models;
-
-public class ApiResponse<T>
-{
-    public bool Success { get; set; }
-    public string Message { get; set; } = "";
-    public int StatusCode { get; set; }
-    public T? Data { get; set; }
-}
